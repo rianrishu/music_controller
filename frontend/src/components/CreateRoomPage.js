feb423049819ae4ede731dc75ea7812b60018770
Rishu Anand <rishuanand2552@gmail.com>
2022-04-15 15:55:36 +0530

    stut6

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,4 +1,14 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoomPage extends Component {
   constructor(props) {
@@ -6,6 +16,12 @@ export default class CreateRoomPage extends Component {
   }
 
   render() {
-    return <p>This is the create room page</p>;
+    return(
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography>Create a Room</Typography>
+        </Grid>
+      </Grid>
+    ) 
   }
 }
