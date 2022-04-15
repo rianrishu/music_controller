cbe607e132beefaf3e0da9e6f9a2794edb56225a
Rishu Anand <rishuanand2552@gmail.com>
2022-04-15 13:44:16 +0530

    reset

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+from dataclasses import field
 from rest_framework import serializers
 from .models import Room
 
@@ -7,3 +8,9 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause',
                   'votes_to_skip', 'created_at')
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')  
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RoomView
+from .views import CreateRoomView, RoomView
 
 urlpatterns = [
     path('room', RoomView.as_view()),
+    path('create', CreateRoomView.as_view()),
 ]
